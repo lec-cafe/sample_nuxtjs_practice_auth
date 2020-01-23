491a5f7a8d6483b6bf82c6e20ee0a49a34040812
mikakane <t.goto@chatbox-inc.com>
2020-01-23 23:06:25 +0900

    udpate

--- pages/mypage/index.vue
+++ pages/mypage/index.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
-    <section>
-      <h2>ようこそ、ユーザ さん</h2>
+    <section v-if="user">
+      <h2>ようこそ、{{ user.display_name }} さん</h2>
     </section>
   </div>
 </template>
 
 <script>
   export default {
+    computed: {
+      user() {
+        return this.$store.state.user.user
+      }
+    },
+    mounted() {
+      if (!this.user) {
+        alert("ログインしてください。")
+        this.$router.push("/")
+      }
+    }
   }
 </script>
 
