73a7c9e607121640cc353a113defd3fe3156df6e
mikakane <t.goto@chatbox-inc.com>
2020-01-27 00:57:39 +0900

    updete

--- plugins/auth.js
+++ plugins/auth.js
@@ -7,15 +7,13 @@ export default ({ store }, inject) => {
         return promise
       }else{
         promise = new Promise(async (resolve)=>{
-          if(store.state.user.user && store.state.user.user.display_name){
+          if(store.state.user.user){
             return store.state.user.user
           }
           if(promise){
             return promise
           }
-          await true
-          await true
-          const result = await  store.dispatch("user/relogin")
+          const result = await store.dispatch("user/relogin")
           return resolve(result)
         })
         return promise
